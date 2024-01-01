3d441ea8561df34e00b3e959164e6f98dc0f5cdc
devmarufurrahaman <mdmarufur5682@gmail.com>
2024-01-01 23:20:57 +0600

    update

--- js/main.js
+++ js/main.js
@@ -224,4 +224,15 @@ document.addEventListener("keydown", function (e) {
 
 tabsContainer.addEventListener("click", function (e) {
 	const btn = e.target.closest(".operations__tab");
+
+	if (!btn) return;
+	tabs.forEach((tab) => tab.classList.remove("operations__tab--active"));
+	tabsContent.forEach((content) =>
+		content.classList.remove("operations__content--active"),
+	);
+
+	btn.classList.add("operations__tab--active");
+	document
+		.querySelector(`.operations__content--${btn.dataset.tab}`)
+		.classList.add("operations__content--active");
 });
